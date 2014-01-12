b2ac12c80b143f82bc7a6dd21891f195772b5d07
rick michael sullivan <rsulliva@linux60818.dc.engr.scu.edu>
2014-01-11 22:23:57 -0800

    Initial commit

---
+++ phase1/examples/tricky.c
@@ -0,0 +1,15 @@
+/* tricky.c */
+
+/***
+ *** a tricky comment ******/
+
+int main(void)
+{
+    int a, b, c;
+
+    a = 0123;
+    b = 123;
+    c = 0x123aF;
+
+    printf("I said \"hello there\"");
+}
