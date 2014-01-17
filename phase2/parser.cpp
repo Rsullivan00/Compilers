13f56c256e6cb602d459026bec1b2483a08d767e
rick michael sullivan <rsulliva@linux60811.dc.engr.scu.edu>
2014-01-17 15:24:35 -0800

    Work on parser

--- phase2/parser.cpp
+++ phase2/parser.cpp
@@ -1,45 +1,110 @@
-#include <iostream>
-#include "tokens.h"
+# include <iostream>
+# include <string>
+# include <stdio.h>
+# include "lexer.h"
+# include "tokens.h"
+
+using namespace std;
+
+int lookahead;
 
 void expression(){
-	
 }
 
 void logicalOr() {
+	logicalAnd();
 
+	while(lookheahead == OROR) {
+		match(OROR);
+		logicalAnd();
+	}	
 }
 
 void logicalAnd() {
+	equality();
 
+	while(lookahead == ANDAND) {
+		match(ANDAND);
+		equality();
+	}	
 }
 
 void equality() {
+	relational();
 
+	while(lookahead == EQUALEQUAL || lookahead == NOTEQUAL) {
+		match(lookahead);
+		relational();
+	}
 }
 
 void relational() {
+	additive();
 
+	while(lookahead == LESSTHANEQUAL || lookahead == GREATERTHANEQUAL ||
+		lookahead == LESSTHAN || lookahead == GREATERTHAN) {
+		match(lookahead);
+		additive();
+	}
 }
 
 void additive() {
+	multiplicative();
 
+	while(lookahead == '+' || lookahead == '-') {
+		match(lookahead);
+		multiplicative();
+	}
 }
 
 void multiplicative() {
+	unary();
 
+	while(lookahead == '*' || lookahead == '/' || lookahead == '%') {
+		match(lookahead);
+		unary();
+	}
 }
 
 void unary() {
+	while(lookahead == '!' || lookahead == '-' || lookahead == '&' ||
+		lookahead == '*' || lookahead == SIZEOF) {
+		match(lookahead);
+	}
 
+	index();	
+}
+
+void index() {
+	while(lookahead == '[') {
+		match('[');
+		expression();
+		match(']');
+	}
+
+	primary();
 }
 
 void primary() {
-
+	if (lookahead == '(') {
+		match('{');
+		expression();
+		match(')');
+	} else if (lookahead == NUM) {
+		match(NUM);
+	} else if (lookahead == ID) {
+		match(ID);
+		// Add id(expression) here
+	} else if (lookahead == STRING) {
+		match(STRING);
+	} else if (	
 } 
 
 void expressionList() {
 
 }
+
+
 /*
  *  * Function:	report
  *   *
@@ -52,15 +117,25 @@ void expressionList() {
 
 void report(const string &str, const string &arg)
 {
-    char buf[1000];
+    	char buf[1000];
 
-    snprintf(buf, sizeof(buf), str.c_str(), arg.c_str());
-    cerr << "line " << yylineno << ": " << buf << endl;
+    	snprintf(buf, sizeof(buf), str.c_str(), arg.c_str());
+    	cerr << "line " << yylineno << ": " << buf << endl;
 }
+
+void match(int t) {
+	if (lookahead == t) {
+		lookahead = yylex();
+	} else { 
+		report("Token not matched: ");			
+	}
+}
+
 
 int main()
 {
-    yylex();
-    return 0;
+    	lookahead = yylex();
+	expression();
+    	return 0;
 }
 
